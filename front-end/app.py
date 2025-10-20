7a9bc19007520edf97321b40a7c29b738b062dde
Igor Oliveira <igoroliveirapupo3@gmail.com>
2025-10-20 13:20:28 -0300

    add o menu catalogo ✔

--- front-end/app.py
+++ front-end/app.py
@@ -8,3 +8,12 @@ st.title(" Gerenciador de Produtos e Estoque")
 
 menu = st.sidebar.radio("Navegação", ["Catalogo", "Adicionar produto", "Atualizar produto"])
 
+if menu == "Catalogo":
+    st.subheader("Todos os produtos disponiveis")
+    response = requests.get(f"{API_URL}/estoque")
+    if response.status_code == 200:
+        produto = response.json().get("produto", [])
+        if produto:
+            st.dataframe(produto)
+    else:
+        st.error("Erro ao tentar acessar a API")
