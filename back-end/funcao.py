176aa22a619aa4dd96c0442d76560e3520a965fe
Igor Oliveira <igoroliveirapupo3@gmail.com>
2025-10-20 10:18:13 -0300

    add a função listar produtos e trocado decimal para numeric

--- back-end/funcao.py
+++ back-end/funcao.py
@@ -9,7 +9,7 @@ def criar_tabela():
             id SERIAL PRIMARY KEY,
             nome VARCHAR(100) NOT NULL,
             categoria VARCHAR(50),
-            preco DECIMAL(10,2),
+            preco NUMERIC (10,2),
             quantidade INT                   
             )
             """)
@@ -31,6 +31,18 @@ def cadastrar_produtos(nome, categoria, preco, quantidade):
             conexao.commit()
         except Exception as erro:
             print(f'Erro ao cadastrar produto {erro}')
+        finally:
+            cursor.close()
+            conexao.close()
+
+def listar_produtos():
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute("SELECT * FROM produtos ORDER BY id")
+            return cursor.fetchall()
+        except Exception as erro:
+             print(f'Erro ao tentar visualizar o estoque {erro}')
         finally:
             cursor.close()
             conexao.close()
